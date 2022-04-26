8473e36369147afee51f1c302368632245fb0eaf
Saud Shah <mian@sauds-mbp.myfiosgateway.com>
2022-04-26 18:47:04 -0400

    Fetching data periodically from API instead of reading local JSON file

--- App.js
+++ App.js
@@ -3,23 +3,55 @@ import { StyleSheet, Text, View, FlatList} from 'react-native';
 import coins from './coins.json';
 import Entry from './components/Entry';
 import Columns from './components/Columns';
+import axios from 'axios';
 
 export default function App() {
 
   const [loading, setLoading] = useState(true);
+  const [coinData, setCoinData] = useState([]);
+
+
+  const getCoins = async () =>{
+    const options = {
+      method:'GET',
+      headers: {
+        Accept: "application/json",
+      }
+    }
+      const coins = await fetch('https://api.coinlore.net/api/tickers/', options)
+      const coinsJson = await coins.json()
+      console.log(coinsJson)
+      setCoinData(...coinData, coinsJson.data)
+      
+  };
 
   useEffect(() => {
+    getCoins()
     setLoading(false)
-}, []);
 
+    const interval = setInterval(() => {
+      getCoins()
+
+    },10000)
+
+    return()=>clearInterval(interval)
+
+   
+},[]);
+   
+    
     return(
       <View style={styles.container}>
-        {loading ? <Text>Loading</Text>:(
-        <View style={styles.card}>
-          <Columns />
-          <FlatList data={coins.data} keyExtractor={coins.data.id} renderItem={Entry} />
-        </View>
+        
+
+        {loading ? <Text style={{fontSize:100,alignSelf:'center',}}>Loading</Text>: (
+           <View style={styles.card}>
+           <Columns />
+           <FlatList data={coinData} keyExtractor={(item) => item.id} renderItem={Entry} />
+         </View>
+
         )}
+        
      </View>
 
     )
