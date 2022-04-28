ebf89b660372686fb6268170a4c5ef0a73541d86
Saud Shah <mian@Sauds-MacBook-Pro.local>
2022-04-28 16:06:04 -0400

    Search Bar Functionality Implemented

--- screens/Detail.js
+++ screens/Detail.js
@@ -4,16 +4,27 @@ import {View,Text,StyleSheet} from 'react-native';
 const Detail = ({route, navigation}) => {
     const {item} = route.params;
     console.log(item)
+    let details =  {'Name':item.name,
+          'Rank':item.rank,
+          'Price USD':item. price_usd,
+          'Percentage Change 24hrs':item.percent_change_24h,
+          'Percentage Change 1hr':item.percent_change_1h,
+          'Percentage Change 7d':item.percent_change_7d,
+          'Price in BTC':item.price_btc,
+          'Market Cap USA':item.market_cap_usd}
+
    
     return (
         <View style={styles.container}>
-             {Object.entries(item).map(([id,value]) => {
+             {Object.entries(details).map(([id,value]) => {
                 return(
                 <View key={id} style={styles.idVals}>
                     <Text style={styles.id}>{id}</Text>
                     <Text style={styles.val}>{value}</Text>
-
+                    
                  </View>
+
+                 
                 )
             })}
         </View>
@@ -30,8 +41,9 @@ const styles= StyleSheet.create({
         paddingVertical:25,
         justifyContent:'space-between',
         alignItems:'center',
-        backgroundColor:'#1a1b1c',
+        backgroundColor:'grey',
         borderWidth:10,
+        borderColor:'whitesmoke',
         borderBottomColor:'whitesmoke'
     },
     id:{
