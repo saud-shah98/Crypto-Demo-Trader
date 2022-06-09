69f51ad7145015a11f37a7ef4570ba29301628ff
saud-shah98 <shahm7@vcu.edu>
2022-06-08 23:20:55 -0500

    Fix Button Size on Register Page

--- components/ButtonBox.js
+++ components/ButtonBox.js
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function ButtonBox({children,spacing}) {
+export default function ButtonBox({ children, spacing }) {
   return (
-      <View style={{ height: 50, width: '85%', marginTop: spacing>0 ? spacing: 0, alignItems:'center' }}>
-
-    {children}
+    <View
+      style={{
+        height: 50,
+        width: "100%",
+        paddingVertical: 10,
+        alignItems: "center",
+      }}
+    >
+      {children}
     </View>
-  )
+  );
 }
