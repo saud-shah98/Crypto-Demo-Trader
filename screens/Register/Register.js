a5390b52ba2f25e4ad5bb34845aa0e47c3278999
saud-shah98 <shahm7@vcu.edu>
2022-06-25 16:46:47 -0500

    Fixed missing react dependency

--- screens/Register/Register.js
+++ screens/Register/Register.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StyleSheet, Text, KeyboardAvoidingView, Platform } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import InputField from "../../components/InputField";
