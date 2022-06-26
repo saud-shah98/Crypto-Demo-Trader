fb34087ce9a1016aca1c81ecb9f85d8ecb64802a
saud-shah98 <shahm7@vcu.edu>
2022-06-26 14:48:50 -0500

    Fix Registration Screen

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
-
 import InventoryEntry from "../../components/InventoryEntry";
 import ProfileHeader from "../../components/ProfileHeader";
 import Logout from "../../components/Logout";
